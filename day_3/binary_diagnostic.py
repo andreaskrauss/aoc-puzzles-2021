d1a80fdef7d12e9c7e94556c257a8822c4087d3e
Andreas Krauss <am.krauss@web.de>
2021-12-19 18:46:48 +0100

    task: add inputs for several riddles

--- day_3/binary_diagnostic.py
+++ day_3/binary_diagnostic.py
@@ -1,36 +1,63 @@
-def analyze_from_matrix(path:str):
-    with open(path) as file:
-        # read matrix and rotate by 90 degree
-        error_matrix = zip(*reversed([line.strip() for line in file.readlines()]))
+class Submarine:
+    diagnostic_report: list[str]
+    gamma: str
+    epsilon: str
+    oxygen: str
+    scubber: str
 
-    gamma = ""
-    epsilon = ""
+    consumption_rating: int
+    life_support_rating: int
 
-    for bit in error_matrix:
-        # determine common bit
-        if bit.count('1') > len(bit)/2:
-            gamma = gamma + '1'
-            epsilon = epsilon + '0'
-        else:
-            gamma = gamma + '0'
-            epsilon = epsilon + '1'
+    def __init__(self, path: str):
+        with open(path) as file:
+            self.diagnostic_report = [line.strip() for line in file.readlines()]
+        self.gamma = ""
+        self.epsilon = ""
+        self.oxygen = ""
+        self.scubber = ""
+        self.consumption_rating = 0
+        self.life_support_rating = 0
 
-    # multiply epsilon and gamma (as decimal) to gain data about power consumption
-    power_consumption = int(gamma, 2) * int(epsilon, 2)
+    def check_power_consumption(self):
+        error_matrix = zip(*reversed(self.diagnostic_report))
 
-    # format result
-    param_results = f"Gamma: {gamma} | Epsilon: {epsilon}"
-    divider = len(param_results)*'-'
-    consumption_result = f"Power Consumption: {power_consumption}"
-    # print result
-    print(param_results + "\n" + divider + "\n" + (abs(len(divider)-len(consumption_result)))*" " + consumption_result)
+        gamma = ""
+        epsilon = ""
+
+        for bit in error_matrix:
+            # determine common bit
+            if bit.count('1') >= len(bit) / 2:
+                gamma = gamma + '1'
+                epsilon = epsilon + '0'
+            else:
+                gamma = gamma + '0'
+                epsilon = epsilon + '1'
+
+        # update key figures
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.consumption_rating = int(gamma, 2) * int(epsilon, 2)
+
+    def check_life_support(self):
+        # for each position
+            # iterate i to point on bit
+            # estimate common bit
+            # filter all strings in logs
+            # next position
+        pass
+
+    def printReport(self):
+        print("System analysis\n"+ 35*"=" + "\n"
+              + f"Epsilon:\t\t\t\t\t{self.epsilon}\n"
+              + f"Gamma:\t\t\t\t\t\t{self.gamma}\n"
+              + f"Oxygen Generator Rating:\t{self.oxygen}\n"
+              + f"CO2 Scubber Rating:\t{self.scubber}\n"
+              + 35 * "=" + "\n"
+              + f"Power Consumption Rating:\t{self.consumption_rating}\n"
+              + f"Life Support Rating:\t\t{self.life_support_rating}\n")
 
 
 if __name__ == '__main__':
-    analyze_from_matrix("input.txt")
-
-
-
-
-
-
+    subby = Submarine("trial_input.txt")
+    subby.check_power_consumption()
+    subby.printReport()
